b33e3b0481e302e3841a3b87cd3ed80bbb0e866a
Ethereal55 <davidbaymanov@gmail.com>
2024-01-30 17:48:13 +0300

    Сделал нормальное управление
    зафризил вращения по осям, теперь джойстик нормально работает в 3д пространстве, без раскручивания вселенной (выглядило как Made in heaven)

--- Pizda/Assets/Player/PlayerController.cs
+++ Pizda/Assets/Player/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float speed;
+    public Joystick joystick;
+    private Rigidbody rb;
 
     private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float dirX = joystick.Horizontal * speed;
+        float dirZ = joystick.Vertical * speed;
+
+        rb.velocity = new Vector3(dirX, 0 , dirZ);
     }
 }
